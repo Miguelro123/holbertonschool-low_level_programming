31b3406b9b6dc63d4a3facf38fc2ad5215d646d6
Miguelro123 <969@holbertonschool.com>
2019-06-20 22:47:58 +0000

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,11 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 65 ; i < 123 ; i++)
+	for (i = 97 ; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+	for(i = 65 ; i < 91 ; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
 	return (0);
 }
-
