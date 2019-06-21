e2f37901b4976b1f1d04c00a0c87efd284b38856
Miguelro123 <969@holbertonschool.com>
2019-06-21 19:14:19 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char nl = '\n';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+
+		putchar(c);
+
+	}
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+
+		putchar(c);
+	}
+}
