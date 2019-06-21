9b52faa60e098f81f702649197d945b00dae2cad
Miguelro123 <969@holbertonschool.com>
2019-06-21 17:20:41 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,15 @@
 int main(void)
 {
 	int c;
-	int nl = '\n';
 
-	for (c = '0'; c <= '9'; ++c)
+	for (c = 48; c <= 57; ++c)
 
 	{
-		putchar(c + '0');
+		putchar(c);
 
 	}
 
-	putchar(nl);
+	putchar('\n');
 
 	return (0);
 }
