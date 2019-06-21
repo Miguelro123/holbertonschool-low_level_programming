76717d31e7a493cd60522ae0385ad97c78ec7dcb
Miguelro123 <969@holbertonschool.com>
2019-06-21 18:14:14 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char nl = '\n';
+
+	for (c = 'z'; c <= 'a'; --c)
+	{
+
+		putchar(c);
+
+	}
+
+	putchar(nl);
+
+	return (0);
+}
