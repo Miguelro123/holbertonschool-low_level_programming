683fd89a21154d7ccd114da6f4eddbf3d01c9642
Miguelro123 <969@holbertonschool.com>
2019-06-22 03:31:58 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,27 +4,28 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	char c;
 	char d;
 
-	for (c = '0' ; c <= '9' ; c++)
+	for (c = '0' ; c = '9' ; c++)
+
 	{
 
 		putchar(c);
 
 	}
 
-	for (d = 'a' ; d <= 'z' ; d++)
+	for (d = 'a' ; d = 'z' ; d++)
+
 	{
 
 		putchar(d);
 	}
+
 	putchar('\n');
 	return (0);
 }
-
