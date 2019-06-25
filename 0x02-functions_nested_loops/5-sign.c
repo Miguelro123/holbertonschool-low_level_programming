e959b915bbcac8cdf177ecf7412553fe11882201
Miguelro123 <969@holbertonschool.com>
2019-06-25 19:32:48 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * print_sign - prints the sign of a number.
+ * @n: variable type int
  * Return: Always 0.
  */
 int print_sign(int n)
