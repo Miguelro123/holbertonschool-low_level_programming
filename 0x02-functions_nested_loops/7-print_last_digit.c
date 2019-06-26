88204e85ead7fe2258a91f0ea07c8cd0b93ac702
Miguelro123 <969@holbertonschool.com>
2019-06-26 01:06:17 +0000

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_last_digit - prints the last digit of a number.
+ * @n: variable type int.
+ * Return: 0.
  */
 int print_last_digit(int n)
 {
