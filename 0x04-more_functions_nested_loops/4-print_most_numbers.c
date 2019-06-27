c9a70e97711ac59b54acb3c7040cc0422954cb12
Miguelro123 <969@holbertonschool.com>
2019-06-27 21:17:05 +0000

    Do not print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - Print numbers 0 to 9, minus 2 and 4.
- * Description: use only _putchar
+ * print_most_numbers - Prints numbers from 0 to 9, Do not print 2 and 4
+ *
  */
 void print_most_numbers(void)
 {
