e723301126dcc996e9a86313e9e4a62968be0f4b
Miguelro123 <969@holbertonschool.com>
2019-07-08 21:15:35 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,15 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int x;
+	unsigned int i = 0;
 
-	for (x = 0 ; x < n ; x++)
+	if (n > i)
 	{
-		*(s + x) = b;
+		while (i < n)
+		{
+			s[i] = b;
+			i++;
+		}
 	}
 	return (s);
 }
