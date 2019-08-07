ac2be6cbb105bd826d0ebbe6c814ae43aa868a24
Miguelro123 <969@holbertonschool.com>
2019-08-07 02:57:58 +0000

    Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef _LISTS_
 #define _LISTS_
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
