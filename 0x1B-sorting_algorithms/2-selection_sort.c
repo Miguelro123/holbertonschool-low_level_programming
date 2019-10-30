81862c25760e89dda7df7bbe91522c1e6a19995c
Miguelro123 <969@holbertonschool.com>
2019-10-30 03:01:24 +0000

    add validation for print

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,26 +1,35 @@
 #include "sort.h"
+
 /**
- * selection_sort - Sort an array using selection sort algorithm
- * @array: input array
- * @size: size of array
+ * selection_sort - sorts an array of integers in asc order
+ *
+ * @array: array of integers
+ * @size: size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t minimo = 0, i, j;
+	size_t i;
+	size_t j;
+	size_t min = 0;
 	int swap;
 
 	if (size > 0 && array != NULL)
 	{
-		for (i = 0 ; i < size - 1 ; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			minimo = i;
-			for (j = i + 1 ; j < size ; j++)
-				if (array[minimo] > array[j])
-					minimo = j;
-			swap = array[minimo];
-			array[minimo] = array[i];
-			array[i] = swap;
-			print_array(array, size);
+			min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[min] > array[j])
+					min = j;
+			}
+			swap = array[min];
+			array[min] = array[i];
+			if (array[i] != swap)
+			{
+				array[i] = swap;
+				print_array(array, size);
+			}
 		}
 	}
 }
