f0db4befaddb24d7561e29020ca4339d200036c0
Miguelro123 <969@holbertonschool.com>
2019-10-30 02:43:00 +0000

    Selection sort algorithm

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -9,16 +9,18 @@ void selection_sort(int *array, size_t size)
 	size_t minimo = 0, i, j;
 	int swap;
 
-	for (i = 0 ; i < size - 1 ; i++)
+	if (size > 0 && array != NULL)
 	{
-		minimo = i;
-
-		for (j = i + 1 ; j < size ; j++)
-			if (array[minimo] > array[j])
-				minimo = j;
-		swap = array[minimo];
-		array[minimo] = array[i];
-		array[i] = swap;
-		print_array(array, size);
+		for (i = 0 ; i < size - 1 ; i++)
+			{
+				minimo = i;
+				for (j = i + 1 ; j < size ; j++)
+					if (array[minimo] > array[j])
+						minimo = j;
+				swap = array[minimo];
+				array[minimo] = array[i];
+				array[i] = swap;
+				print_array(array, size);
+			}
 	}
 }
